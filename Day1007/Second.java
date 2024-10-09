82933d65905e7ddf742b455e119f81b4e11bb4f9
ud <139339681+undo001@users.noreply.github.com>
2024-10-10 08:49:38 +0900

    Add files via upload

---
+++ Day1007/Second.java
@@ -0,0 +1,10 @@
+
+public class Second {
+
+	public static void main(String[] args) {
+		System.out.print("대한민국");
+		System.out.println(10 + 5);
+		System.out.println("10 - 5 = " + (10-5));
+	}
+
+}
